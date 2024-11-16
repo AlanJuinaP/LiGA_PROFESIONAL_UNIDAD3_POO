0a848baecf515a18f0fc2206d2c049f73c0efcad
AlanJuinaP <ajuinap@est.ups.edu.ec>
2024-11-15 23:10:26 -0500

    Actualizacion de la clase Equipo

--- src/Partido.java
+++ src/Partido.java
@@ -20,17 +20,11 @@ public class Partido {
         return equipo1;
     }
 
-    public void setEquipo1(Equipo equipo1) {
-        this.equipo1 = equipo1;
-    }
 
     public Equipo getEquipo2() {
         return equipo2;
     }
 
-    public void setEquipo2(Equipo equipo2) {
-        this.equipo2 = equipo2;
-    }
 
     //metodo para representar el partido como una cadena de texto
     @Override
